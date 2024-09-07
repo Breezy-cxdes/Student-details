71701932d08707882e83d21f01f34cf329d01aef
Breezy-cxdes <snowbreezy20@gmail.com>
2024-09-07 15:18:42 +0200

    initial commit

--- main.py
+++ main.py
@@ -17,3 +17,44 @@ x = Student("Joey", "King", 2021)
 x.printname()
 print(x.graduationyear)
 
+
+#import neccessary package
+from abc import ABC,abstractmethod
+#create a base class
+class animal (ABC):
+    
+    #Abstract method 
+      #sgould be implemented by all sub-classes
+      def move(self):
+               pass
+
+class Animal:
+    def move(self):
+        print("I can move")
+
+class Human(Animal):
+    def move(self):
+        print("I can walk and run")
+
+class Snake(Animal):
+    def move(self):
+        print("I can crawl")
+
+class Dog(Animal):
+    def move(self):
+        print("I can bark")
+
+# Driver code
+R = Human()
+R.move()  # Output: I can walk and run
+
+K = Snake()
+K.move()  # Output: I can crawl
+
+R = Snake()
+R.move()  # Output: I can crawl
+
+K = Dog()
+K.move()  # Output: I can bark
+
+
